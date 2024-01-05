e64e47da1caff872f7a13db64408a259a7dc28e7
Kryptonite19 <oguzhankeles199813@gmail.com>
2024-01-05 10:24:40 +0300

    Deleted redundant code

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <v-app class="custom-font">
-    <v-layout class="full-height">
+    <v-layout>
       <app-bar />
-      <v-main class="d-flex align-center justify-center" style="min-height: 300px">
+      <v-main>
         <router-view />
       </v-main>
     </v-layout>
@@ -14,7 +14,5 @@ import AppBar from './components/AppBar.vue'
 import { RouterView } from 'vue-router'
 </script>
 <style lang="scss" scoped>
-// Import your SCSS file that includes the Source Code Pro font
-@import "./styles/settings.scss";
-
+@import './styles/settings.scss';
 </style>
