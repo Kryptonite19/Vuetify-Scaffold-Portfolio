1897fd7613b266203a8557b79ce7d8762510521c
Kryptonite19 <oguzhankeles199813@gmail.com>
2024-01-05 10:23:28 +0300

    About View Added

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="text-h1">{{ t('$vuetify.navbar.aboutme') }}</div>
+  <div class="pa-5">
+    <div class="text-h3">{{ t('$vuetify.navbar.aboutme') }}</div>
+    <v-img class="bg-[#121212]" max-width="500" src="../assets/DSC_1098.JPEG" cover />
+    <div class="d-flex align-center justify-space-between">
+      <p class="text-justify text-body-1 mt-5">
+        {{ t('$vuetify.about.aboutparagraph') }}
+      </p>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
