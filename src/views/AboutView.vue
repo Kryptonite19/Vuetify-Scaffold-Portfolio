383d17ab35f7285e4ed841007d32cc7fd413f5a8
Kryptonite19 <oguzhankeles199813@gmail.com>
2024-01-05 12:31:19 +0300

    Mİnor layout fix on about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="pa-5">
-    <div class="text-h3">{{ t('$vuetify.navbar.aboutme') }}</div>
-    <v-img class="bg-[#121212]" max-width="500" src="../assets/DSC_1098.JPEG" cover />
-    <div class="d-flex align-center justify-space-between">
-      <p class="text-justify text-body-1 mt-5">
-        {{ t('$vuetify.about.aboutparagraph') }}
-      </p>
-    </div>
-  </div>
+  <v-container>
+    <v-responsive>
+      <div class="text-h3 font-weight-bold">{{ t('$vuetify.navbar.aboutme') }}</div>
+      <v-img class="bg-[#121212]" max-width="500" src="../assets/DSC_1098.JPEG" cover />
+      <div class="d-flex align-center justify-space-between">
+        <p class="text-justify text-body-1 mt-5">
+          {{ t('$vuetify.about.aboutparagraph') }}
+        </p>
+      </div>
+    </v-responsive>
+  </v-container>
 </template>
 
 <script setup lang="ts">
