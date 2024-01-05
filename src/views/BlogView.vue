e60ceca61c7daa08d75c01d34020893369eb9872
Kryptonite19 <oguzhankeles199813@gmail.com>
2024-01-05 10:43:53 +0300

    Blog under construction added

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="text-h1">{{ t('$vuetify.navbar.blog') }}</div>
+  <v-container class="fill-height">
+    <v-row>
+      <v-col>
+        <div class="text-h1">Oops!</div>
+        <div class="text-h3">This page is under construction.</div>
+      </v-col>
+      <v-img v-if="!mobile" aspect-ratio="1" max-width="400" src="../assets/const.png" />
+    </v-row>
+  </v-container>
 </template>
 
 <script setup lang="ts">
-import { useLocale } from 'vuetify'
-
-const { t } = useLocale()
+import { useDisplay } from 'vuetify'
+const { mobile } = useDisplay()
 </script>
