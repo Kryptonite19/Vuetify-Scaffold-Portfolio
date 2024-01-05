5a1305a790981b8bacdacd820af0b975c48e7e67
Kryptonite19 <oguzhankeles199813@gmail.com>
2024-01-05 10:25:04 +0300

    Added about paragraph to locales

--- src/locales/tr.ts
+++ src/locales/tr.ts
@@ -8,5 +8,8 @@ export default {
     settings: 'Ayarlar',
     lightmode: 'Aydınlık Tema',
     darkmode: 'Karanlık Tema'
+  },
+  about: {
+    aboutparagraph: 'Merhaba! Ben Oğuzhan KELEŞ, Ankara, Türkiye merkezli bir web geliştiriciyim. Başkent Üniversitesi Bilgisayar Mühendisliği lisans derecemle donanmış bir şekilde, iş zekası alanında bir şirkette edindiğim bir yıllık web geliştirme deneyimimle buradayım. Uzmanlık alanlarım arasında frontend ve UI/UX tasarımı yer alıyor ve buna ek olarak backend geliştirmeye olan duyarlılığımı da ekliyorum. Iş hayatım dışında, beni doğada kamp yaparken veya bilardo oynarken bulabilirsiniz. Hayatta stoacılık felsefesine inanıyor ve "En iyisini yap, gerisini akışa bırak" mottosuyla yaşıyorum. Mükemmel olmamaktan doğan eksiklikleri kucaklıyor ve bunları büyümenin bir parçası olarak görüyorum. Elimden geleni yapmaya odaklanır ve gerisinin peşinde koşmamaya çalışırım. Geliştirici olarak, becerilerimi Vue.js üzerinde yoğunlaştırdım ve dinamik ve duyarlı kullanıcı arayüzleri oluşturmak için bu alanda uzmanlık kazandım. Sürekli öğrenmeye adanmış biriyim ve şu anda web framework uzmanlığımı genişletmeye odaklanarak teknolojinin sürekli değişen manzarasında daha etkili olmayı hedefliyorum. Haydi birlikte çalışalım ve olağanüstü dijital deneyimler yaratmaya başlayalım!'
   }
 }
