980189af110675512eb64734020896587f16baa5
Kryptonite19 <oguzhankeles199813@gmail.com>
2024-01-06 23:26:39 +0300

    Added Contact Page Locales

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -1,4 +1,5 @@
 export default {
+  herotext: 'Welcome to my portfolio website where i tell you about myself !',
   navbar: {
     home: 'Home',
     aboutme: 'About Me',
@@ -11,5 +12,13 @@ export default {
   },
   about: {
     aboutparagraph: 'Hello! I`m Oğuzhan KELEŞ, a web developer based in Ankara, Turkey. Armed with a bachelor`s degree in Computer Engineering from Başkent University and one year of web development experience in the field of business intelligence, I bring a well-rounded skill set to the table. My expertise lies in frontend development and UI/UX design, and I also have a keen interest in backend development. Embracing imperfection, I see shortcomings as part of the growth process. As a developer, I`ve focused my skills on Vue.js, gaining expertise in creating dynamic and responsive user interfaces. I am dedicated to continuous learning and currently aim to enhance my web framework proficiency to be more effective in the ever-changing landscape of technology.'
+  },
+  contactpage: {
+    yourmail: 'Your E-Mail',
+    subject: 'Subject',
+    message: 'Your Message',
+    submit: 'SUBMIT',
+    connect: 'Let`s Connect',
+    connectbody: 'I`m currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I`ll try my best to get back to you!'
   }
 }
--- src/locales/tr.ts
+++ src/locales/tr.ts
@@ -1,4 +1,5 @@
 export default {
+  herotext: 'Sizlere kendimden bahsettiğim portfolyo websiteme hoşgeliniz !',
   navbar: {
     home: 'Ana Sayfa',
     aboutme: 'Hakkımda',
@@ -11,5 +12,13 @@ export default {
   },
   about: {
     aboutparagraph: 'Merhaba! Ben Oğuzhan KELEŞ, Ankara, Türkiye merkezli bir web geliştiriciyim. Başkent Üniversitesi Bilgisayar Mühendisliği lisans derecemle donanmış bir şekilde, iş zekası alanında bir şirkette edindiğim bir yıllık web geliştirme deneyimimle buradayım. Uzmanlık alanlarım arasında frontend ve UI/UX tasarımı yer alıyor ve buna ek olarak backend geliştirmeye olan duyarlılığımı da ekliyorum. Mükemmel olmamaktan doğan eksiklikleri kucaklıyor ve bunları büyümenin bir parçası olarak görüyorum. Geliştirici olarak, becerilerimi Vue.js üzerinde yoğunlaştırdım ve dinamik ve duyarlı kullanıcı arayüzleri oluşturmak için bu alanda uzmanlık kazandım. Sürekli öğrenmeye adanmış biriyim ve şu anda web framework uzmanlığımı genişletmeye odaklanarak teknolojinin sürekli değişen manzarasında daha etkili olmayı hedefliyorum.'
+  },
+  contactpage: {
+    yourmail: 'E-Mail Adresiniz',
+    subject: 'Konu',
+    message: 'İletiniz',
+    submit: 'Gönder',
+    connect: 'İrtibata geçelim',
+    connectbody: 'Şu anda yeni fırsatlar arıyorum, e-posta kutum herkese açık. Bir sorunuz varsa veya sadece merhaba demek istiyorsanız, çekinmeden bana ulaşın size en kısa sürede geri dönmeye çalışacağım.'
   }
 }
